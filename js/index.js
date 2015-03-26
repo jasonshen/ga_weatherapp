63dd50df3015463965a8a7536e86f2305f1d965b
Jason Shen <jasonyshen@gmail.com>
2015-03-26 19:51:38 -0400

    Was able to grab the json file correctly

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 
 $(function () {
 
+	var city = "London"
+	$("#getweather").click(function () {
+   
+		$.ajax("http://api.openweathermap.org/data/2.5/weather?q="+city, {
+		    dataType: "jsonp",
+		    success: function (weatherData) {
+		        console.log(weatherData.main.temp_max);
+		        console.log("Something!")
+
+		    }
+		})
+	})
+	
+
+
 });
