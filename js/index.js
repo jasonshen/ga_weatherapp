f0753d7371046db8ebf4b0c3e29cfa8c66a4196d
Jason Shen <jasonyshen@gmail.com>
2015-03-26 21:10:41 -0400

    Got current and forecasted weather working, with conversion to F

--- js/index.js
+++ js/index.js
@@ -16,11 +16,23 @@ $(function () {
 			        console.log(weatherData.main.temp_max);
 			        console.log("Something!")
 			        console.log(weatherData.weather[0].description);
+			        $("#city-name").text(weatherData.name);
 			        $("#weather-status").text(weatherData.weather[0].description);
 			        $("#current-temp").text(weatherData.main.temp);
 			        $("#wind-speed").text(weatherData.wind.speed);
-			        console.log(convertK(weatherData.main.temp))
+			        $("#current-temp").text(convertK(weatherData.main.temp));
 			    }
+			})
+
+
+			$.ajax("http://api.openweathermap.org/data/2.5/forecast/daily?q="+city,
+			 {
+			    dataType: "jsonp",
+			    success: function (weatherForecast) {
+			        $("#tomorrow").text(weatherForecast.list[0].weather[0].description);
+			        $("#2-days").text(weatherForecast.list[1].weather[0].description);
+			    }
+
 
 			})
 
@@ -30,7 +42,8 @@ $(function () {
 	})
 
 	function convertK (k) {
-		return ((k - 273.15)* 1.8000 + 32.00)
+		temp = ((k - 273.15)* 1.8 + 32);	
+		return temp.toFixed(1);
 	}
 	
 
