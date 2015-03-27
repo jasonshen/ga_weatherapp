9b1c51298df29a20b971eaf9e027f4a69cd7dd81
Jason Shen <jasonyshen@gmail.com>
2015-03-26 20:38:05 -0400

    Correctly calling the JSON file for current weather

--- js/index.js
+++ js/index.js
@@ -2,17 +2,36 @@
 $(function () {
 
 	var city = "London"
-	$("#getweather").click(function () {
-   
-		$.ajax("http://api.openweathermap.org/data/2.5/weather?q="+city, {
-		    dataType: "jsonp",
-		    success: function (weatherData) {
-		        console.log(weatherData.main.temp_max);
-		        console.log("Something!")
 
-		    }
-		})
+	city = $("#location").val()
+	console.log(city)
+
+	$("#get-weather").click(function () {
+    	if ($("#location").val() != null) {
+	    	city = $("#location").val()
+
+			$.ajax("http://api.openweathermap.org/data/2.5/weather?q="+city, {
+			    dataType: "jsonp",
+			    success: function (weatherData) {
+			        console.log(weatherData.main.temp_max);
+			        console.log("Something!")
+			        console.log(weatherData.weather[0].description);
+			        $("#weather-status").text(weatherData.weather[0].description);
+			        $("#current-temp").text(weatherData.main.temp);
+			        $("#wind-speed").text(weatherData.wind.speed);
+			        console.log(convertK(weatherData.main.temp))
+			    }
+
+			})
+
+		}
+
+
 	})
+
+	function convertK (k) {
+		return ((k - 273.15)* 1.8000 + 32.00)
+	}
 	
 
 
